d14149fffd9871b2f5a70026e51aa7f7708563a0
Lokesh Patidar <Lokeshpati84@gmail.com>
2024-01-14 19:45:21 +0530

    Total setup of nodeJs

---
+++ nodeServer1/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+
+const app = express();
+
+const USERS = [
+    {
+      "id": 1,
+      "email": "george.bluth@reqres.in",
+      "first_name": "George",
+      "last_name": "Bluth",
+      "avatar": "https://reqres.in/img/faces/1-image.jpg"
+    },
+    {
+      "id": 2,
+      "email": "janet.weaver@reqres.in",
+      "first_name": "Janet",
+      "last_name": "Weaver",
+      "avatar": "https://reqres.in/img/faces/2-image.jpg"
+    },
+    {
+      "id": 3,
+      "email": "emma.wong@reqres.in",
+      "first_name": "Emma",
+      "last_name": "Wong",
+      "avatar": "https://reqres.in/img/faces/3-image.jpg"
+    },
+    {
+      "id": 4,
+      "email": "eve.holt@reqres.in",
+      "first_name": "Eve",
+      "last_name": "Holt",
+      "avatar": "https://reqres.in/img/faces/4-image.jpg"
+    },
+    {
+      "id": 5,
+      "email": "charles.morris@reqres.in",
+      "first_name": "Charles",
+      "last_name": "Morris",
+      "avatar": "https://reqres.in/img/faces/5-image.jpg"
+    },
+    {
+      "id": 6,
+      "email": "tracey.ramos@reqres.in",
+      "first_name": "Tracey",
+      "last_name": "Ramos",
+      "avatar": "https://reqres.in/img/faces/6-image.jpg"
+    }
+  ]
+
+app.get("/", (req, res) => {
+  res.send("Its Working now");
+});
+
+
+app.get('/users', (req, res) => {
+    res.json(USERS);
+})
+
+
+app.listen(3000, () => {
+  console.log("Server runing succesfully on LocalHost 3000");
+});
